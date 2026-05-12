b57840b51600e8a0f0c247a24c75d58b5ae7de96
Soto <sofia.soto74@eia.edu.co>
2026-05-12 15:51:16 -0500

    Lógica backtracking completa

--- Tablero.java
+++ Tablero.java
@@ -1,51 +1,94 @@
-package Backtracking;
+package Proyecto;
+
+import java.util.ArrayList;
 
 public class Tablero {
 	private int dimTablero;
 	private int[] posicionDamas;
+	private boolean casillaFija = false;
+	private boolean nValido = true;
+	int cont = 0;
 	
-	public Tablero(int dimension, int colInicial) {
+	public Tablero(int dimension) {
 		dimTablero = dimension;
 		posicionDamas= new int[dimension];
-		posicionDamas[0] = colInicial;
-		for(int i = 1; i<dimension; i++) {
+		for(int i = 0; i<dimension; i++) {
 			posicionDamas[i] = -1;
 		}
 	}
 	
-	public void ubicarDamas() {
-		int i = 0;
-		while (i<dimTablero && posicionDamas[i]!=-1) i++;
-		if(i == dimTablero) {
-			imprimirTablero();
-			System.out.println();
-		} else if (!validarDama(0, i)) {
-			System.out.println("Esa no es una posición válida");
-		} else ubicarDamas();
-		
+	public void setCasillaFija(int colInicial) {
+		casillaFija=true;
+		posicionDamas[0]=colInicial;
 	}
 	
-	public boolean validarDama(int columna, int fila) {
-		if (fila<1) return false;
-		if (columna < dimTablero && (!validarColumna(columna, fila) || !validarDiagonal(columna, fila))) {
-			posicionDamas[fila] = -1;
-			return validarDama(columna+1, fila);
-		} else if (columna>=dimTablero) {
-			posicionDamas[fila] = -1;
-			return validarDama(posicionDamas[fila-1] +1, fila-1);
-		} else {
-			posicionDamas[fila]=columna;
-			return true;
+	public int[][] inicializarPrimera() {
+		return primeraSolucionDamas(casillaFija?1:0,0);
+	}
+	
+	public int[][] primeraSolucionDamas(int fila, int colI) {
+		while(fila>=(casillaFija?1:0) && fila<dimTablero && validarFila(fila,colI)) {
+			fila++;
+			colI = 0;
 		}
+		if (fila==dimTablero) {
+			return imprimirTablero();
+		}
+		fila--;
+		if (fila <= (casillaFija?0:-1)) {
+			if(casillaFija) {
+				System.out.println("Casilla o N no válida");
+				return null;
+			} else {
+				System.out.println("N no válida");
+				return null;
+			}
+		}
+		int temp = posicionDamas[fila];
+		posicionDamas[fila]=-1;
+		return primeraSolucionDamas(fila, temp+1);
+	}
+	
+	public ArrayList<int[][]> inicializarTodas() {
+		ArrayList<int[][]> soluciones = new ArrayList<int[][]>();
+		return encontrarSoluciones(0, soluciones);
+	}
+	
+	private ArrayList<int[][]> encontrarSoluciones(int fila, ArrayList<int[][]> soluciones) {
+		if (fila == dimTablero) {
+			++cont;
+			soluciones.add(imprimirTablero());
+		}
+		
+		for (int columna = 0; columna < dimTablero; columna++) {
+				if (validarColumna(columna, fila) && validarDiagonal(columna, fila)) {
+				posicionDamas[fila] = columna;
+				encontrarSoluciones(fila + 1, soluciones);
+				posicionDamas[fila] = -1;
+			}
+		}
+		return soluciones;
+	}
+	
+	public boolean validarFila(int fila, int columna) {
+		while(columna<dimTablero && (!validarColumna(columna, fila) || !validarDiagonal(columna, fila))) {
+			columna++;
+		}
+		if (columna==dimTablero) {
+			posicionDamas[fila]=-1;
+			return false;
+		}
+		posicionDamas[fila]=columna;
+		return true;
 	}
 	
 	public boolean validarColumna(int columna, int fila) {
 		int i = 0;
 		while (i<fila && posicionDamas[i]!=columna) {
 			i++;
-		}
-		if(i==fila) return true;
-		return false;
+		}	
+		if(posicionDamas[i]==columna) return false;
+		return true;
 	}
 	
 	public boolean validarDiagonal(int columna, int fila) {
@@ -53,22 +96,23 @@ public class Tablero {
 		while (j<fila && fila-j!=Math.abs(columna-posicionDamas[j])) {
 			j++;
 		}
-		if(j==fila) return true;
-		return false;
+		if(fila-j==Math.abs(columna-posicionDamas[j])) return false;
+		return true;
 	}
 	
-	public void imprimirTablero() {
+	public int[][] imprimirTablero() {
+		int[][] tablero = new int[dimTablero][dimTablero];
 		for(int i = 0; i<dimTablero; i++) {
 			for (int j = 0; j<dimTablero; j++) {
 				if (j==posicionDamas[i]) {
-					System.out.print(1 + " ");
-				} else System.out.print(0 + " ");
+					tablero[i][j]=1;
+				} else tablero[i][j]=0;
 			}
-			System.out.println();
 		}
+		return tablero; 
 	}
 	public static void main(String[] args) {
-		Tablero tablero = new Tablero(6, -1);
-		tablero.ubicarDamas();
+		Tablero tablero = new Tablero(4);
+		tablero.inicializarPrimera();
 	}
 }
